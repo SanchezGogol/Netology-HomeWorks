22ff7f9632209b93bff59a092460ce3c28297121
SanchezGogol <153436724+SanchezGogol@users.noreply.github.com>
2024-02-19 21:38:51 +0300

    edit params

--- ter-homerworks/03/src/disk_vm.tf
+++ ter-homerworks/03/src/disk_vm.tf
@@ -10,30 +10,30 @@ resource "yandex_compute_disk" "disk_vm" {
 
 
 resource "yandex_compute_instance" "storage" {
-name = "storage"
-    platform_id = var.platform_id_v
-    resources {
+  name = "storage"
+  platform_id = var.platform_id_v
+  resources {
       cores         = 2
       memory        = 1
       core_fraction = 5
   }
-    boot_disk {
+  boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
-    dynamic "secondary_disk" {
-    for_each = yandex_compute_disk.disk_vm
-    content {
-      device_name = "secondary-disk-${secondary_disk.key}"
-      disk_id     = secondary_disk.value.id
+  }
+  dynamic secondary_disk {
+      for_each = yandex_compute_disk.disk_vm
+      content {
+        device_name = "secondary-disk-${secondary_disk.key}"
+        disk_id     = "${secondary_disk.value.id}"
+      }
     }
-  }
-  }
-    metadata = {
+  metadata = {
        ssh-keys = local.ssh_key_file
        #"ubuntu:${var.vms_ssh_root_key}"
   }
-    network_interface {
+  network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
     security_group_ids = [yandex_vpc_security_group.example.id]
